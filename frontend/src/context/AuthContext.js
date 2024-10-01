c07d86fa07056ba899330d75ad45c136773bc813
Harsh <bhonsleharsh16@gmail.com>
2024-10-01 21:40:03 +0530

    add AuthContext and authContext hook

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,10 +1,16 @@
 import {createContext, useReducer} from 'react'
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
+
 
 export const authReducer =(state, action)=>{
     switch(action.type){
-        
+        case 'LOGIN':
+            return {user: action.payload }
+        case 'LOGOUT':
+            return {user: null}
+        default: 
+        return state
     }
 }
 
@@ -12,4 +18,10 @@ export const AuthContextProvider = ({children}) =>{
     const [state, dispatch] = useReducer(authReducer,{
         user:null
     })
+    console.log('AuthContext state:', state)
+    return (
+        <AuthContext.Provider value ={{...state,dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+const Signup =() =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form className="signup">
+
+        </form>
+    )
+}
